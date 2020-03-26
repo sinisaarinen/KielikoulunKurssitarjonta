664bc7eda1e2c30b99729dce0c751ce21cddb623
Sini <saasini@ad.helsinki.fi>
2020-03-26 15:21:16 +0200

    lisättiin validaattoreita

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
-    name = StringField("Name")
-    username = StringField("Username")
-    password = PasswordField("Password")
+    name = StringField("Name", [validators.Length(min=4)])
+    username = StringField("Username", [validators.Length(min=4)])
+    password = PasswordField("Password", [validators.Length(min=6)])
   
     class Meta:
         csrf = False
