9d33ece367b51f718d9a469c2966532b47e5bf51
Sini <saasini@ad.helsinki.fi>
2020-04-01 11:14:42 +0300

    abstraktin luokan lisääminen

--- application/courses/models.py
+++ application/courses/models.py
@@ -1,11 +1,8 @@
 from application import db
+from application.models import Base
 
-class Course(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-    onupdate=db.func.current_timestamp())
-
+class Course(Base):
+    
     name = db.Column(db.String(144), nullable=False)
     coursecode = db.Column(db.String(144), nullable=False)
     language = db.Column(db.String(144), nullable=False)
