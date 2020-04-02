d835eb9d11d8563d366dbc4acfda8ed5013712f0
Sini <saasini@ad.helsinki.fi>
2020-04-02 14:02:17 +0300

    korjailua

--- application/courses/views.py
+++ application/courses/views.py
@@ -67,7 +67,7 @@ def courses_create():
         return render_template("courses/new.html", form = form)
 
     course = Course(form.name.data, form.coursecode.data, form.language.data, \
-        form.level.data, form.spots.data, form.course_location.data, form.description.data, form.registrationsopen.data)
+        form.level.data, form.spots.data, form.course_location.data.id, form.description.data, form.registrationsopen.data)
     course.registrationsopen = form.registrationsopen.data
     db.session().add(course)
     db.session().commit()
